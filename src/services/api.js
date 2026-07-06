f99a44beb636b18546647ff17522fec4dd6343ab
Fabian <0333232021@unjfsc.edu.pe>
2026-07-06 14:30:03 -0500

    Configuracion Ngrok para consumo de API

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,15 @@
 import axios from 'axios';
 
+//ngrok temporalS
+const baseURL = import.meta.env.VITE_API_URL
 // Creamos una instancia base de Axios
 const api = axios.create({
-    baseURL: 'http://localhost:8081/api'
+    baseURL: baseURL,
+    headers: {
+    'Content-Type': 'application/json',
+    // ¡ESTA LÍNEA ES CRUCIAL PARA NGROK!
+    'ngrok-skip-browser-warning': 'true' 
+    }
 });
 
 // ¡EL INTERCEPTOR! 
